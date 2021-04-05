7db72c9faf228b0ab94fca4a77ada4c0043712ec
samuellevy <samuellevyjr@gmail.com>
2021-04-05 16:43:31 -0300

    Pequena melhoria no UI.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -25,9 +24,15 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(1,0));
-		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
+		 placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+	     placeNewPiece('b', 1, new Rook(board, Color.WHITE));
+	     
+	     placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+	     
 	}
 }
